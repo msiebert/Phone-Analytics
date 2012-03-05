f85b85c99459474c42c11e78c6d2ed35369c0072
Mark Siebert <mark.jiali.siebert@gmail.com>
2012-03-05 13:35:37 -0700

    Added the class to read the Mission Organization from an Excel file.

---
+++ src/excel/MissionOrganization.java
@@ -0,0 +1,20 @@
+package excel;
+
+import java.util.Set;
+
+/*
+ * This class simply contains a static method that will read the Mission Organization out of 
+ * an Excel file
+ * */
+
+public class MissionOrganization {
+	
+	/*This method reads the file MissionOrganization.xls and gets the mission's organization
+	 * from the file.
+	 * RETURN VALUE: Set = a set of sets which represent zones holding maps which represent areas
+	 * */
+	public static Set getMissionOrganization() {
+		return null;
+	}
+	
+}
